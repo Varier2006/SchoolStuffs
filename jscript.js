33fe035ad7d2c06de925b81ccf4973fdd739d92d
Varier2006 <matus.capo11@gmail.com>
2022-04-18 18:23:53 +0200

    Feature added: Switching between stylesheets

--- jscript.js
+++ jscript.js
@@ -8,30 +8,41 @@ function executeButtonReadJson() {
     let button = document.getElementById("buttonReadJson")
     button.addEventListener("click", readJson)
 }
-
 function executeButtonShowTopDiv() {
     let button = document.getElementById("buttonShowTopDiv")
     button.addEventListener("click", showTopDiv)
 }
-
+function executeButtonThemeToggle(){
+    let button = document.getElementById("buttonThemeToggle")
+    button.addEventListener("click", themeSwitch)
+}
 function divDragging() {
     let div = document.getElementById("topDiv")
     div.addEventListener("drag", hideTopDiv)
 }
-
 function showTopDiv(){
     let container = document.querySelector(".container")
     container.style.display="block"
     let button = document.querySelector("#buttonShowTopDiv")
     button.style.display="none"
 }
-
+function themeSwitch(){
+    // Obtains an array of all <link>
+    // elements.
+    // Select your element using indexing.
+    let theme = document.getElementsByTagName('link')[0];
+    if (theme.getAttribute('href') === 'light.css') {
+        theme.setAttribute('href', 'dark.css');
+    }
+    else {
+        theme.setAttribute('href', 'light.css');
+    }
+}
 const fetchJson = async ()=>{
     let name = "names.json"
     const response = await fetch(name)
     return await response.json()
 }
-
 function readJson(){
     let container = document.querySelector(".container")
     let json = fetchJson()
@@ -50,3 +61,4 @@ function readJson(){
 executeButtonReadJson()
 divDragging()
 executeButtonShowTopDiv()
+executeButtonThemeToggle()
